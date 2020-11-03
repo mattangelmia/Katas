a4a4d49333efc04c8541ff18b7f0ef9b0a65acfe
mattangelmia <23matthewangel@gmail.com>
2020-11-03 17:09:19 -0500

    consequetive

--- index.js
+++ index.js
@@ -23,3 +23,28 @@ function disemvowel(str) {
 
 
 }
+
+
+
+
+// You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
+
+
+function consecutive(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === a && arr[i + 1] === b) {
+            return true
+        }
+        else if (arr[i] === b && arr[i + 1] === a) {
+            return true
+        }
+        else if (arr[i] === a && arr[i + 1] !== b) {
+            return false
+        }
+        else if (arr[i] === b && arr[i + 1] !== a) {
+            return false
+        }
+    }
+}
