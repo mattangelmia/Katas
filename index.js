191c3effab0e659948e770eabcea8a3ac6c8d902
mattangelmia <23matthewangel@gmail.com>
2020-11-05 14:48:38 -0500

    newkata

--- index.js
+++ index.js
@@ -84,3 +84,17 @@ function checkExam(correctAnswers, studentAnswers) {
     };
     return score;
 };
+
+
+
+// Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+// For example: 
+// solve("abc") = True, because it contains a,b,c
+// solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+// solve("dabc") = True, because it contains a, b, c, d
+// solve("abbc") = False, because b does not occur once.
+// solve("v") = True
+
+
+function solve(s) { return "abcdefghijklmnopqrstuvwxyz".includes([...s].sort().join``) }
